d7b992081db236ad917ff70a5b4cfd0fd32fc5fe
Gaspard Benoit <gaspardbenoit@msn.com>
2014-12-23 13:55:55 +0100

    Uncomment

--- echo.sh
+++ echo.sh
@@ -3,11 +3,11 @@
 
 echo "Configuring wifi credentials"
 
-# uci set wireless.@wifi-iface[0].ssid="$1"
-# uci set wireless.@wifi-iface[0].encryption="$2"
-# uci set wireless.@wifi-iface[0].key="$3"
-# uci commit wireless
-# wifi
+uci set wireless.@wifi-iface[0].ssid="$1"
+uci set wireless.@wifi-iface[0].encryption="$2"
+uci set wireless.@wifi-iface[0].key="$3"
+uci commit wireless
+wifi
 
-echo "$1" $1 $2 $3
+# echo "$1" $1 $2 $3
 
